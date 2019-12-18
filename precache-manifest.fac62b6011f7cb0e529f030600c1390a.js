b7a7217f4f6eacd523c7339a357ec0a2603ec520
Steven <swli222@uky.edu>
2019-12-18 15:45:06 -0500

    Updates

---
+++ precache-manifest.fac62b6011f7cb0e529f030600c1390a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de10231ce7fd016942ecb94e7c9f6750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451973e542373983a0b3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a692176a3515ce2d3c75",
+    "url": "/static/js/2.0a5bd8f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0a5bd8f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "451973e542373983a0b3",
+    "url": "/static/js/main.8234a127.chunk.js"
+  },
+  {
+    "revision": "b47e9a02d859e174c93e",
+    "url": "/static/js/runtime-main.86a20dad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
